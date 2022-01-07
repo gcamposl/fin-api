6e602a88a396c9bdf5065f68cb5dff246caec715
Gabriel <gcamposlopes@gmail.com>
2022-01-07 10:13:04 -0300

    criando rota de criação de contas e definindo os dados do cliente

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const express = require("express");
-
+const { v4: uuidv4 } = require("uuid"); // renomeada
 const app = express();
+app.use(express.json());
+
+const costumers = []; //bd fake
+/*
+* CPF - string
+* name - string
+* id - uuid
+* statement - []
+*/
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4(); // gera um id randomico 
+
+  costumers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  return response.status(201).send();
+})
 
 app.listen(3333);
