d6304b3a619ed11cf262d10f50c83f842c09cbdd
Gabriel <gcamposlopes@gmail.com>
2022-01-10 23:15:17 -0300

    criando deposito na conta.

--- src/index.js
+++ src/index.js
@@ -48,4 +48,21 @@ app.get("/statement/", veriftIfExistsAccountCPF, (request, response) => {
   return response.json(customer.statement);
 });
 
+app.post("/deposit", veriftIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
 app.listen(3333);
